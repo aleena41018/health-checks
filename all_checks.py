54aa0ec7d2d0e30a38423cfde5483546aa64ba07
Aleena Mini Varghese <aleenaminikts5@gmail.com>
2020-05-16 11:04:43 +0530

    Added a new feature to print all the errors in a computer.

--- all_checks.py
+++ all_checks.py
@@ -1,13 +1,13 @@
 import shutil
 import sys
-import os  
+import os 
 
 def check_reboot():
     return os.path.exists("/run/reboot-required")
 
 def check_root_full():
     return check_disk_usage("/", 2, 10)
-    
+
 def check_disk_usage(disk, min_gb, min_percent):
     du = shutil.disk_usage(disk)
     percent_free = 100 * du.free / du.total
@@ -17,13 +17,13 @@ def check_disk_usage(disk, min_gb, min_percent):
     return True
 
 def main():
-    checks = [ (check_reboot, "Pending Reboot!"), (check_root_full, "Root partition full!"), ]
+    checks = [ (check_reboot, "Pending reboot!!"), (check_disk_usage, "Root partition Full!!"), ]
     everything_ok=True
-    for check, msg in checks:
+    if check, msg in checks:
         if check():
             print(msg)
             everything_ok=False
     if not everything_ok:
-        sys.exit(1)
+        sys.exit(0)
     print("Everything OK!")
     sys.exit(0)
