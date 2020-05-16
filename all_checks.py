c923dcb92e1707948c625dd9f157e6ac44d6c77e
Aleena Mini Varghese <aleenaminikts5@gmail.com>
2020-05-16 10:46:30 +0530

    Save changed!

--- all_checks.py
+++ all_checks.py
@@ -1,10 +1,13 @@
 import shutil
 import sys
-import os 
+import os  
 
 def check_reboot():
     return os.path.exists("/run/reboot-required")
 
+def check_root_full():
+    return check_disk_usage("/", 2, 10)
+    
 def check_disk_usage(disk, min_gb, min_percent):
     du = shutil.disk_usage(disk)
     percent_free = 100 * du.free / du.total
@@ -14,11 +17,13 @@ def check_disk_usage(disk, min_gb, min_percent):
     return True
 
 def main():
-    if check_reboot():
-        print("Pending reboot!!")
-        sys.exit(1)
-    if not check_disk_usage("/", 2, 10):
-        print("Disk Full!!")
+    checks = [ (check_reboot, "Pending Reboot!"), (check_root_full, "Root partition full!"), ]
+    everything_ok=True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            everything_ok=False
+    if not everything_ok:
         sys.exit(1)
     print("Everything OK!")
     sys.exit(0)
